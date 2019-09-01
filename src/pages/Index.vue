98f01a60fa0f38eae1854328d76489329b632e98
xhostcom <support@xhostcom.com>
2019-09-01 19:03:51 +0100

    Update

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -12,7 +12,61 @@
     <div class="bg-circle-2 bg-circle"></div>
     <div class="bg-circle-3 bg-circle"></div>
     <div class="bg-circle-4 bg-circle"></div>
-</header>	
+</header>
+ <section>
+    <div class="container">
+      <div class="row align-items-center">
+        <div class="col-lg-6 order-lg-2">
+          <div class="p-5">
+            <img class="img-fluid rounded-circle" src="../assets/images/01.jpg" alt="01">
+          </div>
+        </div>
+        <div class="col-lg-6 order-lg-1">
+          <div class="p-5">
+            <h2 class="display-4">For those about to create...</h2>
+            <p>Using the Vue Wordpress Gridsome Sass enabled Bootstrap 4 Avivo theme.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section>
+    <div class="container">
+      <div class="row align-items-center">
+        <div class="col-lg-6">
+          <div class="p-5">
+            <img class="img-fluid rounded-circle" src="../assets/images/02.jpg" alt="02">
+          </div>
+        </div>
+        <div class="col-lg-6">
+          <div class="p-5">
+            <h2 class="display-4">We salute you!</h2>
+            <p>You've chosen this customizable Vue Wordpress API Gridsome Jamstack Bootstrap 4 theme to leave your mark on the world!</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section>
+    <div class="container">
+      <div class="row align-items-center">
+        <div class="col-lg-6 order-lg-2">
+          <div class="p-5">
+            <img class="img-fluid rounded-circle" src="../assets/images/03.jpg" alt="03">
+          </div>
+        </div>
+        <div class="col-lg-6 order-lg-1">
+          <div class="p-5">
+            <h2 class="display-4">Let us get creating!</h2>
+            <p>Start here and create somthing awesome to be remebered by!</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+	
 		
 
 </Layout>
